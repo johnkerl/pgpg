3b304486ac6ac614bfda0b8945208d479df4c5a6
John Kerl <kerl.john.r@gmail.com>
2026-02-07 10:55:46 -0500

    neatening

--- v1/pkg/parsers/ame_parser.go
+++ v1/pkg/parsers/ame_parser.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/johnkerl/pgpg/pkg/asts"
 	"github.com/johnkerl/pgpg/pkg/lexers"
+	"github.com/johnkerl/pgpg/pkg/tokens"
 )
 
 // Grammar:
@@ -30,13 +31,15 @@ func (parser *AMEParser) Parse(inputText string) (*asts.AST, error) {
 	if err != nil {
 		return nil, err
 	}
+	if err := parser.expect(tokens.TokenTypeEOF); err != nil {
+		return nil, err
+	}
 	return asts.NewAST(rootNode), nil
 }
 
 func (parser *AMEParser) parseSumOrProduct() (*asts.ASTNode, error) {
 	lookaheadToken := parser.lexer.LookAhead()
 
-	// TODO: methodize
 	if lookaheadToken.IsError() {
 		return nil, errors.New(string(lookaheadToken.Lexeme))
 	}
@@ -60,16 +63,49 @@ func (parser *AMEParser) parseSumOrProduct() (*asts.ASTNode, error) {
 		)
 	}
 
-	acceptedToken := parser.lexer.Advance()
-	opToken := parser.lexer.Advance()
-	if opToken.IsError() {
-		return nil, errors.New(string(opToken.Lexeme))
+	accepted, acceptedToken, err := parser.accept(lexers.AMLexerTypeNumber)
+	if err != nil {
+		return nil, err
+	}
+	if !accepted {
+		return nil, errors.New("AMEParser: expected int literal")
 	}
 
-	if opToken.IsEOF() {
+	lookaheadToken = parser.lexer.LookAhead()
+	if lookaheadToken.IsError() {
+		return nil, errors.New(string(lookaheadToken.Lexeme))
+	}
+
+	if lookaheadToken.IsEOF() {
 		// The entire expression is a single number
 		node := asts.NewASTNode(acceptedToken, nil) // TODO: type
 		return node, nil
+	}
+
+	if lookaheadToken.Type != lexers.AMLexerTypePlus && lookaheadToken.Type != lexers.AMLexerTypeTimes {
+		actualDesc, err := parser.lexer.DecodeType(lookaheadToken.Type)
+		if err != nil {
+			return nil, err
+		}
+		expectedPlus, err := parser.lexer.DecodeType(lexers.AMLexerTypePlus)
+		if err != nil {
+			return nil, err
+		}
+		expectedTimes, err := parser.lexer.DecodeType(lexers.AMLexerTypeTimes)
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf(
+			"AMEParser: expected %s or %s; got %s",
+			expectedPlus,
+			expectedTimes,
+			actualDesc,
+		)
+	}
+
+	opToken := lookaheadToken
+	if err := parser.expect(opToken.Type); err != nil {
+		return nil, err
 	}
 
 	// Make a binary node with parent being the plus or times operator, the left child being the
@@ -82,4 +118,40 @@ func (parser *AMEParser) parseSumOrProduct() (*asts.ASTNode, error) {
 	parent := asts.NewASTNode(opToken, []*asts.ASTNode{leftChild, rightChild}) // TODO: type
 
 	return parent, nil
+}
+
+func (parser *AMEParser) accept(tokenType tokens.TokenType) (bool, *tokens.Token, error) {
+	lookaheadToken := parser.lexer.LookAhead()
+	if lookaheadToken.Type == tokenType {
+		// The current token is of the expected type, but there may or may not have been a lex error
+		// getting the next token
+		lexerr := parser.getAndValidateLookaheadToken()
+		return true, lookaheadToken, lexerr
+	}
+	// The current token is not of the expected type
+	return false, nil, nil
+}
+
+func (parser *AMEParser) expect(tokenType tokens.TokenType) error {
+	accepted, _, lexerr := parser.accept(tokenType)
+	if lexerr != nil {
+		// Lex error getting the next token
+		return lexerr
+	}
+	if !accepted {
+		// No lex error getting the next token, but the current
+		// token isn't of the expected type
+		return errors.New("expect: unexpected symbol") // XXX describe it: expected & actual type and lexeme
+	}
+	return nil
+}
+
+func (parser *AMEParser) getAndValidateLookaheadToken() error {
+	parser.lexer.Advance()
+	lookaheadToken := parser.lexer.LookAhead()
+	if lookaheadToken.IsError() {
+		return errors.New(string(lookaheadToken.Lexeme))
+	}
+
+	return nil
 }
