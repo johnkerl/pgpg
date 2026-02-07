0f4bd5c7dd1e482ada7ddd07747e8482fe603c92
John Kerl <kerl.john.r@gmail.com>
2026-02-07 12:55:45 -0500

    neaten

--- v1/pkg/tokens/location.go
+++ v1/pkg/tokens/location.go
@@ -1,15 +1,5 @@
 package tokens
 
-// TokenLocation contains information for lexer state, namely the ByteOffset, as well
-// as user-facing information in the form of LineNumber and ColumnNumber.
-// A string FileName is not included -- I feel like this is too bulky to keep this for every single
-// token from a given input file. The filename information should be tracked up a level.
-type TokenLocation struct {
-	LineNumber   int
-	ColumnNumber int
-	ByteOffset   int
-}
-
 // NewTokenLocation is the normal use-case for a lexer starting at the beginning of input text.
 func NewTokenLocation() *TokenLocation {
 	return &TokenLocation{
--- v1/pkg/tokens/types.go
+++ v1/pkg/tokens/types.go
@@ -7,6 +7,16 @@ type TokenType string
 const TokenTypeEOF TokenType = "EOF"
 const TokenTypeError TokenType = "ERROR"
 
+// TokenLocation contains information for lexer state, namely the ByteOffset, as well
+// as user-facing information in the form of LineNumber and ColumnNumber.
+// A string FileName is not included -- I feel like this is too bulky to keep this for every single
+// token from a given input file. The filename information should be tracked up a level.
+type TokenLocation struct {
+	LineNumber   int
+	ColumnNumber int
+	ByteOffset   int
+}
+
 // Token tracks a single lexeme and its type (as determined by the lexer) as well as where it was
 // found within the source text.
 type Token struct {
