ea155ffdf3881141f3b9a982c8993e30f956c1ff
John Kerl <kerl.john.r@gmail.com>
2026-02-07 10:59:10 -0500

    pkg/tokens/types.go

--- v1/pkg/tokens/token.go
+++ v1/pkg/tokens/token.go
@@ -4,21 +4,6 @@ import (
 	"fmt"
 )
 
-type TokenType int
-
-// These token-types are common to all lexers. Then, any given lexer impl will have its own
-// (nominally non-negative) types in addition.
-const TokenTypeEOF = -1
-const TokenTypeError = -2
-
-// Token tracks a single lexeme and its type (as determined by the lexer) as well as where it was
-// found within the source text.
-type Token struct {
-	Lexeme   []rune
-	Type     TokenType
-	Location TokenLocation
-}
-
 // NewToken constructs a new token, nominally for a lexer to use while scanning.
 // The location is copied. The idea is that a lexer can keep a TokenLocation in its
 // object state, updated with the LocateRune method, and then on producing a token
