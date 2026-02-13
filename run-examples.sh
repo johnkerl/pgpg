bf417720291cb2e3e7cceaa1f8e3dd1744e69ea7
John Kerl <kerl.john.r@gmail.com>
2026-02-12 23:45:56 -0500

    seng example

--- run-examples.sh
+++ run-examples.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+echo; echo ----------------------------------------------------------------
 echo; ./runners/tryparse m:vic     expr 'x = x + 1'
 echo; ./runners/tryparse m:vbc     expr 'a AND b OR c AND d'
 echo; ./runners/tryparse m:pemdas  expr '1*2+3'
@@ -12,7 +13,9 @@ echo; ./runners/tryparse g:stmts   expr 'print(1);y=2; if(x=3)y=4;'
 echo; ./runners/tryparse g:lisp    expr '(+ 1 (* 2 3) (* 4 5)) ; comment here'
 
 echo; echo ----------------------------------------------------------------
+echo; ./runners/tryparse g:seng expr 'the red cat quickly jumps over the green dog'
 
+echo; echo ----------------------------------------------------------------
 echo; ./runners/tryparse g:json    expr '[]'
 echo; ./runners/tryparse g:json    expr '[1]'
 echo; ./runners/tryparse g:json    expr '[1,2]'
