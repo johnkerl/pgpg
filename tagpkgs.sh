3c73aaa4bf8570b94d22547b485902bb4d7fcd27
John Kerl <kerl.john.r@gmail.com>
2026-02-28 10:40:18 -0500

    tagpkgs.sh

--- tagpkgs.sh
+++ tagpkgs.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "$0: need tag argument in the form v0.1.0" 1>&2
+us=$(basename $0)
+usage() {
+  echo "Usage: $us {version}" 1>&2
+  echo "Version must match vN.N.N (e.g. v1.2.3)" 1>&2
   exit 1
+}
+
+if [ $# -ne 1 ]; then
+  usage
 fi
 version="$1"
-git tag $version
-git tag go/$version
-git push origin $version
-git push origin go/$version
+
+if ! [[ "$version" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "$(basename "$0"): invalid version '$version'" 1>&2
+  usage
+fi
+
+git tag    "$version"
+git tag "go/$version"
+git push origin    "$version"
+git push origin "go/$version"
